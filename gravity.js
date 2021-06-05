22ec30529694a9b8f2842ec04e0f9921725b56e5
jimmyrhythm <80177614+jimmyrhythm@users.noreply.github.com>
2021-06-05 16:53:39 -0400

    test1

--- gravity.js
+++ gravity.js
@@ -1,5 +1,10 @@
 AFRAME.registerComponent('gravity', {
-   schema:{ floor:{type: 'number',default:0 } },
+   schema:{
+      floor:{type: 'number',default:0 } 
+      Vx:{type: 'number',default:0 } 
+      Vy:{type: 'number',default:0 } 
+      Vz:{type: 'number',default:0 } 
+},
   init: function () {
     var el = this.el;
       var name=  el.getAttribute('id');
@@ -8,5 +13,10 @@ AFRAME.registerComponent('gravity', {
       label.setAttribute('position',"0.5 0.95 0");
       label.setAttribute('class',"nametag");
       el.appendChild(label); 
+  },
+  tick:function(){
+  var prev=this.el.getAttribute('position');
+     console.log(prev);
+  
   }
 });
