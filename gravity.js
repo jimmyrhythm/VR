5d8971a67a4f08d00fd644fdb20a3935e9cba6fa
jimmyrhythm <80177614+jimmyrhythm@users.noreply.github.com>
2021-06-05 16:54:14 -0400

    tag width

--- gravity.js
+++ gravity.js
@@ -9,7 +9,7 @@ AFRAME.registerComponent('gravity', {
     var el = this.el;
       var name=  el.getAttribute('id');
       label=document.createElement('a-entity');
-      label.setAttribute('text',"value: "+ name + ";font:monoid; color:white; width:1 ; ");
+      label.setAttribute('text',"value: "+ name + ";font:monoid; color:white; width:4 ; ");
       label.setAttribute('position',"0.5 0.95 0");
       label.setAttribute('class',"nametag");
       el.appendChild(label); 
