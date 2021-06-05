a5cf1103ba0a42a3aef8320be31c96f79e8e9949
jimmyrhythm <80177614+jimmyrhythm@users.noreply.github.com>
2021-06-05 15:39:26 -0400

    Update gravity.js

--- gravity.js
+++ gravity.js
@@ -1,16 +1,11 @@
-// I said the code was art, I didn't say the code was good. 
-
 AFRAME.registerComponent('gravity', {
- 
-  schema:{ floor:{type: 'number',default:0 } },
-
+   schema:{ floor:{type: 'number',default:0 } },
   init: function () {
     var el = this.el;
       var name=  el.getAttribute('id');
       label=document.createElement('a-entity');
       label.setAttribute('text',"value: "+ name + ";font:monoid; color:white; width:1 ; ");
-      
-      label.setAttribute('position',"0.5 0.35 0");
+      label.setAttribute('position',"0.5 0.95 0");
       label.setAttribute('class',"nametag");
       el.appendChild(label); 
   }
